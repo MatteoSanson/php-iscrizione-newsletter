09e66395d8c7bf1c745ecc4599276e815b5ea1e4
MatteoSanson <matteo.sanson@gmail.com>
2024-01-10 18:53:00 +0100

    feat: milestone3

--- functions.php
+++ functions.php
@@ -8,17 +8,16 @@ function alertEmail() {
     if (isset($_GET["email_utente"])):
         $email_utente = $_GET["email_utente"];
 
-        if (rightEmail($email_utente)):
+        $alertClass = rightEmail($email_utente) ? 'alert-success' : 'alert-danger';
 ?>
-            <div class="mt-3 alert alert-success" role="alert">
+        <div class="mt-3 alert <?php echo $alertClass; ?>" role="alert">
+            <?php if (rightEmail($email_utente)): ?>
                 La tua e-mail è valida: <?php echo $email_utente; ?>
-            </div>
-<?php else: ?>
-            <div class="mt-3 alert alert-danger" role="alert">
+            <?php else: ?>
                 La mail che hai inserito <?php echo $email_utente; ?> non è corretta. Una e-mail valida deve contenere "@" e "."
-            </div>
+            <?php endif; ?>
+        </div>
 <?php
-        endif;
     endif;
 }
 ?>
