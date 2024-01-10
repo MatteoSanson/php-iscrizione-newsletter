2f2129ef32fd85fcb49d859cb0d8362efb4e7cae
MatteoSanson <matteo.sanson@gmail.com>
2024-01-10 16:08:33 +0100

    feat: milestone1

--- index.php
+++ index.php
@@ -11,10 +11,38 @@
 </head>
 <body>
     <header>
-        header
+        <div class="container text-center p-4">
+        <h1>PHP Iscrizione Newsletter</h1>
+        </div>
     </header>
     <main>
-        main
+        <div class="container">
+            <form action="index.php" method="GET">
+                <div class="mb-3">
+                    <label for="email_utente" class="form-label">Inserisci un indirizzo e-mail di registrazione</label>
+                    <input type="text" class="form-control" id="email_utente" name="email_utente">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Conferma</button>
+            </form>
+
+            <?php
+                if (isset($_GET["email_utente"])) {
+                    $email_utente = $_GET["email_utente"];
+
+                    // la mail contiene "@" e "."
+                    if (strpos($email_utente, '@') !== false && strpos($email_utente, '.') !== false) {
+                        echo '<div class="mt-3 alert alert-success" role="alert">';
+                        echo 'La tua e-mail è valida: ' . $email_utente;
+                        echo '</div>';
+                    } else {
+                        echo '<div class="mt-3 alert alert-danger" role="alert">';
+                        echo 'La mail che hai inserito ' . $email_utente . ' non è corretta. Una e-mail valida deve contenere "@" e "."';
+                        echo '</div>';
+                    }
+                }
+            ?>
+        </div>
     </main>
 </body>
 </html>
